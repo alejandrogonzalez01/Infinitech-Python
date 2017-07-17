73210e44b843432336b9b628a38df8f725325cc9
alejandrogonzalez01 <alejandrogonzalez01@gmail.com>
2017-07-17 16:46:08 -0700

    Update infinitechConsole V2.py

--- Python/infinitechConsole V2.py
+++ Python/infinitechConsole V2.py
@@ -1,4 +1,5 @@
 import gspread
+import datetime as cal
 from oauth2client.service_account import ServiceAccountCredentials
 
 ### Interactive client with Google API ###
@@ -77,18 +78,12 @@ def list_restaurants():
 
 
 
-############TEST###########
-mem = []        
-        
-
-def list_by_status(S):
-    ## Remove unwanted lines ##
-    global mem
+    
     counter = 0
     while counter < len(data):
         if counter in unwantedIndexes:
             counter += 1
-        else:
+        elif counter not in unwantedIndexes and data[counter] not in mem:
             mem.append(data[counter])
             counter += 1
 
@@ -96,11 +91,133 @@ def list_by_status(S):
         if S in item:
             print item
 
+def menu():
+    firstMenu = True
+    secondMenu = True
 
+    while firstMenu is True:
+        print ''
+        print '1) List all Restaurants'
+        print '2) List by Status'
+        print '3) Add Call Data'
+
+        choice= int(raw_input(":"))
+        if choice == 1:
+            list_restaurants()
+        elif choice == 2:
+            while secondMenu is True:
+                print ''
+                print '1) Ordered'
+                print '2) Pending'
+                print '3) Dead'
+                print '4) Called'
+                
+                choice2 = int(raw_input(":"))
+                lazy_me = [1,2,3,4]
+
+                if choice2 not in lazy_me:
+                    print 'Invalid option'
+                elif choice2 in lazy_me:
+                    choicesStatus = ['ORDERED', 'PENDING', 'DEAD', 'CALLED']
+                    list_by_status(choicesStatus[choice2 - 1])
+                else:
+                    menu()
+
+        elif choice == 3:
+            add_call()
+        else:
+            print 'That option is invalid'
+
+
+def add_call():
+    ##### TEST #####
+    date = '7/17/2017'
+
+    
+    print 'Input data'
+    insert = raw_input(":")
+    input_data = insert.split(',')
+    input_data.insert(0, date)
+    input_data.append('PENDING')
+    sheet.insert_row(input_data, len(data) + 1)
     
 
     
 if __name__ == '__main__':
     parseData()
-    list_by_status('ORDERED')
+    menu()
+
+        
+
+def list_by_status(S):
+    ## Remove unwanted lines ##
+    mem = []
+
     
+    counter = 0
+    while counter < len(data):
+        if counter in unwantedIndexes:
+            counter += 1
+        elif counter not in unwantedIndexes and data[counter] not in mem:
+            mem.append(data[counter])
+            counter += 1
+
+    for item in mem:
+        if S in item:
+            print item
+
+def menu():
+    firstMenu = True
+    secondMenu = True
+
+    while firstMenu is True:
+        print ''
+        print '1) List all Restaurants'
+        print '2) List by Status'
+        print '3) Add Call Data'
+
+        choice= int(raw_input(":"))
+        if choice == 1:
+            list_restaurants()
+        elif choice == 2:
+            while secondMenu is True:
+                print ''
+                print '1) Ordered'
+                print '2) Pending'
+                print '3) Dead'
+                print '4) Called'
+                
+                choice2 = int(raw_input(":"))
+                lazy_me = [1,2,3,4]
+
+                if choice2 not in lazy_me:
+                    print 'Invalid option'
+                elif choice2 in lazy_me:
+                    choicesStatus = ['ORDERED', 'PENDING', 'DEAD', 'CALLED']
+                    list_by_status(choicesStatus[choice2 - 1])
+                else:
+                    menu()
+
+        elif choice == 3:
+            add_call()
+        else:
+            print 'That option is invalid'
+
+
+def add_call():
+    ##### TEST #####
+    date = '7/17/2017'
+
+    
+    print 'Input data'
+    insert = raw_input(":")
+    input_data = insert.split(',')
+    input_data.insert(0, date)
+    input_data.append('PENDING')
+    sheet.insert_row(input_data, len(data) + 1)
+    
+
+    
+if __name__ == '__main__':
+    parseData()
+    menu()
